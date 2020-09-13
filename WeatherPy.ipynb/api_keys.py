068d6fe4a3a8c1c13ffedee74547b23dcf67f5d9
zhanna zhenis <zhannazhenis@MacBook-Pro.attlocal.net>
2020-09-13 00:59:13 -0500

    homework

--- WeatherPy.ipynb/api_keys.py
+++ WeatherPy.ipynb/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "AIzaSyCvs-4PGrynI0Hc2wjpUAIpoIhkX9TWGlc"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCvs-4PGrynI0Hc2wjpUAIpoIhkX9TWGlc"
